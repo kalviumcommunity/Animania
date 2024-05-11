3d37d34bb1fe7e9fb279d060eb974f7e3c05182b
NihitYadav <nihit.yadav@kalvium.community>
2024-05-12 03:05:53 +0530

    Remove the hardcoded API key and used environment variables  with RAPID_API_KEY and implemented more specific error handling for /anime endpoint

--- server.js
+++ server.js
@@ -4,39 +4,44 @@ const port = process.env.PORT || 3000;
 const axios = require('axios');
 const cors = require('cors');
 
-app.use(express.json())
-
+app.use(express.json());
 app.use(cors());
 
 app.get('/ping', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.get('/anime/:name', async(req, res) => {
-try {
-    console.log(req.params);
-    const options = {
-        method: 'GET',
-        url: 'https://anime-db.p.rapidapi.com/anime',
-        params: {
-          page: '1',
-          size: '10',
-          search: req.params.name,
-          genres: 'Fantasy,Drama',
-          sortBy: 'ranking',
-          sortOrder: 'asc'
-        },
-        headers: {
-            'X-RapidAPI-Key': '409cba03ebmsh4d778f8d697531bp1d06dbjsn85322a08cfbc',
-            'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+app.get('/anime', async (req, res) => {
+    try {
+        const options = {
+            method: 'GET',
+            url: 'https://anime-db.p.rapidapi.com/anime',
+            params: {
+                page: '1',
+                size: '10',
+                // search: req.params.name,
+                genres: 'Fantasy,Drama',
+                sortBy: 'ranking',
+                sortOrder: 'asc'
+            },
+            headers: {
+                'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+                'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+            }
+        };
+        
+        const response = await axios.request(options);
+        res.send(response.data);
+    } catch (error) {
+        console.error(error);
+        if (error.response && error.response.status) {
+            // Client-side error
+            res.status(error.response.status).json({ error: 'Client-side error occurred.' });
+        } else {
+            // Server-side error
+            res.status(500).json({ error: 'Internal server error occurred.' });
         }
-      };
-	const response = await axios.request(options);
-	// console.log(response.data);
-    res.send(response.data)
-} catch (error) {
-	console.error(error);
-}
+    }
 });
 
 const server = app.listen(port, () => {
